a183f8458482299149ce18465e5e9906a3910e4e
andry19 <andry2391@gmail.com>
2017-01-18 12:20:17 +0100

    naj bi delo javascript za eimail/email še ne dela

---
+++ SpletnaDrazba/Migrations/201701181029179_NekMigration.cs
@@ -0,0 +1,16 @@
+namespace SpletnaDrazba.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NekMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
